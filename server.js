0a42ddb86a562c31890a0dd8b07ae9d4ffc6e365
Vanessa Bell <vanessa.bell14@gmail.com>
2016-10-18 15:10:13 -0700

    port 5000

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use('/client/static',express.static(__dirname + "/client/static"));
 //app settings
 app.set('views',path.join(__dirname, './client/views'));
 app.set('view engine','ejs');
+app.set('port', (process.env.PORT || 5000));
 
 require('./server/config/mongoose.js');
 
@@ -18,7 +19,7 @@ var routes_setter = require('./server/config/routes.js');
 routes_setter(app);
 
 // Setting Server to Listen on Port: 5000
-app.listen(5000, function() {
+app.listen(app.get('port'), function() {
     console.log("otter-dashboard listening on port 5000");
 })
 
